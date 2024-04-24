04dc8bf34b8161b1d405b2965129b9a00a36b2cb
TrymFH <trym.fjosne.hexeberg@gmail.com>
2024-04-24 20:33:19 +0200

    lagde controller og la midlertidige løsninger for arraylist osv

--- src/main/java/com/example/webprogoblig3/KinoController.java
+++ src/main/java/com/example/webprogoblig3/KinoController.java
@@ -1,9 +1,28 @@
 package com.example.webprogoblig3;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class KinoController {
 
+    List<Billett> bilettListe = new ArrayList<>();
+    @GetMapping ("/lagre")
+    public void lagreBillett (Billett innBillett){
+        bilettListe.add(innBillett);
+    }
 
-}
+    @PostMapping("/hentAlle")
+    public List<Billett> hentAlle (){
+        return bilettListe;
+    }
+
+    @GetMapping("/slettAlle")
+    public void Slettalle (){
+        bilettListe.clear();
+    }
+ }
